e792c934b4a852870ebdc9922cc7bd3028da1895
Aseel Abusal <aseel.abosul@gmail.com>
2019-01-19 20:14:52 +0200

    Updates

---
+++ precache-manifest.7a4f5f77dff4e3b7777a9e5534423a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotsearch/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b6e15b8d6096113096bc",
+    "url": "/robotsearch/static/js/runtime~main.b6e15b8d.js"
+  },
+  {
+    "revision": "91ed6ba355b2e3a2b0e2",
+    "url": "/robotsearch/static/js/main.91ed6ba3.chunk.js"
+  },
+  {
+    "revision": "b31febb55d46dcc05ad3",
+    "url": "/robotsearch/static/js/1.b31febb5.chunk.js"
+  },
+  {
+    "revision": "91ed6ba355b2e3a2b0e2",
+    "url": "/robotsearch/static/css/main.d75daf44.chunk.css"
+  },
+  {
+    "revision": "b31febb55d46dcc05ad3",
+    "url": "/robotsearch/static/css/1.663514c5.chunk.css"
+  },
+  {
+    "revision": "741b49a05ab7afbf2451e8f3ea646bd6",
+    "url": "/robotsearch/index.html"
+  }
+];
